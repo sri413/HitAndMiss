00041882c1f79242460d12810ef468a010e1b21b
srinidhi <srindhi@uroc.com>
2024-11-03 16:59:35 +0530

    *Refactored some of the code *Added a helper class to parse Excel.

---
+++ HitAndMiss/ExcelParser.cs
@@ -0,0 +1,84 @@
+﻿using ClosedXML.Excel;
+
+public class ExcelParser
+{
+    public List<int> W_three = new List<int>();
+    public List<int> W_five = new List<int>();
+    public List<double> W_threeWL = new List<double>();
+    public List<double> W_fiveWL = new List<double>();
+    public List<int> M_three = new List<int>();
+    public List<int> M_five = new List<int>();
+    public List<List<int>> Three_Combo = new List<List<int>>();
+    public List<List<int>> Five_Combo = new List<List<int>>();
+    private string xlFilename = "main.xlsx"; // Placeholder for your actual file path
+
+    public static ExcelParser xlParserInstance = null;
+
+    public static ExcelParser GetXlParserInstance
+    {
+        get
+        {
+            if (xlParserInstance == null)
+            {
+                xlParserInstance = new ExcelParser();
+            }
+            return xlParserInstance;
+        }
+    }
+
+
+    public void ReadData()
+    {
+        using (var workbook = new XLWorkbook(xlFilename))
+        {
+            ReadWorksheet(workbook, "Weight_three", "W_THREE", W_three);
+            ReadWorksheet(workbook, "Weight_five", "W_FIVE", W_five);
+
+            ReadWorksheet(workbook, "Weight_three", "THREE_WCOMBO", Three_Combo);
+            ReadWorksheet(workbook, "Weight_five", "FIVE_WCOMBO", Five_Combo);
+
+            ReadWorksheet(workbook, "Three_Turns", "THREE_WINLOSS", W_threeWL);
+            ReadWorksheet(workbook, "Five_Turns", "FIVE_WINLOSS", W_fiveWL);
+
+            ReadWorksheet(workbook, "Three_Turns", "Multiplier_three", M_three);
+            ReadWorksheet(workbook, "Five_Turns", "Multiplier_five", M_five);
+           
+        }
+    }
+
+    private void ReadWorksheet(XLWorkbook workbook, string sheetName, string RangeName, List<int> list)
+    {
+        var worksheet = workbook.Worksheet(sheetName);
+        var range = worksheet.Range(RangeName);
+        foreach (var cell in range.Cells())
+        {
+            list.Add((int)cell.Value);
+        }
+    }
+
+    private void ReadWorksheet(XLWorkbook workbook, string sheetName, string RangeName, List<List<int>> list)
+    {
+        var worksheet = workbook.Worksheet(sheetName);
+        var range = worksheet.Range(RangeName);
+
+        foreach (var row in range.RowsUsed())
+        {
+            var newRow = new List<int>();
+            foreach (var cell in row.CellsUsed())
+            {
+                newRow.Add((int)cell.Value);
+            }
+            list.Add(newRow);
+        }
+    }
+
+    private void ReadWorksheet(XLWorkbook workbook, string sheetName, string RangeName, List<double> list)
+    {
+        var worksheet = workbook.Worksheet(sheetName);
+        var range = worksheet.Range(RangeName);
+        foreach (var cell in range.Cells())
+        {
+            list.Add((double)cell.Value);
+        }
+    }
+}
--- HitAndMiss/Program.cs
+++ HitAndMiss/Program.cs
@@ -1,56 +1,31 @@
-﻿#region Assembly ClosedXML, Version=0.102.2.0, Culture=neutral, PublicKeyToken=fd1eb21b62ae805b
-// C:\Users\abhay\.nuget\packages\closedxml\0.102.2\lib\netstandard2.1\ClosedXML.dll
-// Decompiled with ICSharpCode.Decompiler 8.1.1.7464
-#endregion
-
-using System.Linq;
-using ClosedXML.Excel;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using MathNet.Numerics.Random;
-using MathNet.Numerics.Distributions;
-using MathNet.Numerics.Statistics;
-using System.Linq;
-using DocumentFormat.OpenXml.Office.Word;
-using System.Security.Principal;
-using System.Diagnostics;
+using ClosedXML.Excel;
 using Irony.Parsing;
-using DocumentFormat.OpenXml.Spreadsheet;
-using DocumentFormat.OpenXml.Office2010.ExcelAc;
-using DocumentFormat.OpenXml.Drawing.Charts;
-using DocumentFormat.OpenXml.Office2010.Excel;
-using DocumentFormat.OpenXml.Office2013.Excel;
-using System.Security.AccessControl;
 
-public class HitAndMiss
+class HitAndMiss
 {
-    public string NumberOfSims;
-    public string Gametype;
+    private double totalWin;
+    private double BetAmount = 1.0; // Assume some default value for BetAmount
     private double GameID;
-    private double Game_type;
-    Random rand = new Random();
+    private string NumberOfSims;
+    private string Gametype;
+    private double RTP;
+    private double WinValue;
+
+    private Dictionary<double, int> PerMultiplierWin = new Dictionary<double, int>();
+    private Random rand = new Random();
+
     List<int> multiplierValue = new List<int>() { 1, 2, 3, 5, 10, 25 };
     List<int> multiplierWeight = new List<int>() { 875000, 50000, 33333, 20833, 12500, 8333 };
-    Dictionary<int, int> PerMultiplierWin = new Dictionary<int, int>();
-    public int WinValue = 0;
-    public int BetAmount = 1;
-    int totalWin = 0;
-    double RTP = 0;
-    public string xlFilename = "main.xlsx";
-    List<int> W_three = new List<int>();
-    List<int> W_five = new List<int>();
-    List<double> W_threeWL = new List<double>();
-    List<double> W_fiveWL = new List<double>();
-    List<int> M_three = new List<int>();
-    List<int> M_five = new List<int>();
-    List<List<int>> three_combo = new List<List<int>>();
-    List<List<int>> five_combo = new List<List<int>>();
+
 
     public static void Main(string[] args)
-    {
-        HitAndMiss hnm = new HitAndMiss();
+    { 
+       
+        ExcelParser.GetXlParserInstance.ReadData();
 
-        hnm.readdata();
+        HitAndMiss hnm = new HitAndMiss();
         hnm.CalculateSpin();
     }
 
@@ -58,104 +33,80 @@ public class HitAndMiss
     {
         Console.WriteLine("Enter number of simulations");
         NumberOfSims = Console.ReadLine();
-        if (NumberOfSims != "")
+        if (double.TryParse(NumberOfSims, out GameID))
         {
-            GameID = double.Parse(NumberOfSims);
+            Console.WriteLine("Enter Type of game to play");
+            Gametype = Console.ReadLine();
+            for (int i = 0; i < GameID; i++)
+            {
+                switch (Gametype)
+                {
+                    case "3":
+                        RNGGenerator(3);
+                        break;
+                    case "5":
+                        RNGGenerator(5);
+                        break;
+                    default:
+                        RNGGenerator(1);
+                        break;
+                }
+            }
 
-        }
-        Console.WriteLine("Enter Type of game to play");
-        Gametype = Console.ReadLine();
-        //Game_type = double.Parse(Gametype);
-        if (Gametype == "3")
-        {
-            for (int i = 0; i < GameID; i++)
+            RTP = totalWin / (GameID * BetAmount);
+            foreach (var i in PerMultiplierWin)
             {
-                RNGGenerator(3);
+                Console.WriteLine("Key = {0}, Value = {1}", i.Key, i.Value);
             }
-        }
-        else if (Gametype == "5")
-        {
-            for (int i = 0; i < GameID; i++)
-            {
-                RNGGenerator(5);
-            }
+            Console.WriteLine("RTP = {0}", RTP);
         }
         else
         {
-            for (int i = 0; i < GameID; i++)
-            {
-                RNGGenerator(1);
-            }
+            Console.WriteLine("Invalid input for number of simulations.");
         }
-        RTP = (totalWin / (GameID * BetAmount));
-        foreach (var i in PerMultiplierWin)
-        {
-            Console.WriteLine("Key = {0}, Value = {1}", i.Key, i.Value);
-        }
-
-        Console.WriteLine("RTP = {0}", RTP);
-
     }
 
-    private void RNGGenerator(int g_type)
+    private void RNGGenerator(int gType)
     {
-        if (g_type == 3)
-        {
-            //int randomNumber = GenerateWeightedRandomNumber(null, W_threeWL);
-            double randomNumber = rand.Next(0, (int)GameID);
+        int randomNumber = rand.Next(0, (int)GameID);
+        double threshold = gType == 3 ? ExcelParser.GetXlParserInstance.W_threeWL[0] * GameID : ExcelParser.GetXlParserInstance.W_fiveWL[0] * GameID;
 
-            if (randomNumber >= (W_threeWL[0] * GameID))
-            {
-                SpinLooseCalculation(3);
-            }
-            else
-            {
-                SpinWinCalculation(3);
-            }
-
-        }
-        else if (g_type == 5)
+        if (gType == 1)
         {
-            //int randomNumber = GenerateWeightedRandomNumber(null, W_fiveWL);
-            double randomNumber = rand.Next(0, (int)GameID);
-
-            if (randomNumber >= (W_threeWL[0] * GameID))
-            {
-                SpinLooseCalculation(5);
-            }
-            else
-            {
-                SpinWinCalculation(5);
-            }
-        }
-        else
-        {
-            int randomNumber = rand.Next(0, (int)GameID);
             if (randomNumber <= 0.6 * GameID)
             {
-                SpinWinCalculation(1);
+                SpinWinCalculation(gType);
             }
             else
             {
-                SpinLooseCalculation(1);
+                SpinLooseCalculation(gType);
+            }
+        }
+        else
+        {
+            if (randomNumber >= threshold)
+            {
+                SpinLooseCalculation(gType);
+            }
+            else
+            {
+                SpinWinCalculation(gType);
             }
         }
     }
 
-    private void SpinWinCalculation(int g_type)
+    private void SpinWinCalculation(int gType)
     {
-
-        if (g_type == 3)
+        if (gType == 3)
         {
-            CalculateWin(null, W_three, M_three);
+            CalculateWin(null, ExcelParser.GetXlParserInstance.W_three, ExcelParser.GetXlParserInstance.M_three);
         }
-        else if (g_type == 5)
+        else if (gType == 5)
         {
-            CalculateWin(null, W_five, M_five);
+            CalculateWin(null, ExcelParser.GetXlParserInstance.W_five, ExcelParser.GetXlParserInstance.M_five);
         }
-        else if(g_type == 1)
+        else if (gType == 1)
         {
-            //CalculateWin(multiplierValue, multiplierWeight, );
             int randomMultiplier = GenerateWeightedRandomNumber(multiplierValue, multiplierWeight);
             WinValue = randomMultiplier * BetAmount;
             totalWin += WinValue;
@@ -166,10 +117,10 @@ public class HitAndMiss
         }
     }
 
-    private void CalculateWin(List<int> multiplierValues ,List<int> WeightCombo, List<int> W_ComboType )
+    private void CalculateWin(List<int> multiplierValues, List<int> WeightCombo, List<int> W_ComboType)
     {
-        int combo_type = GenerateWeightedRandomNumber(multiplierValues, WeightCombo);
-        int multi = W_ComboType[combo_type];
+        int comboType = GenerateWeightedRandomNumber(multiplierValues, WeightCombo);
+        int multi = W_ComboType[comboType];
         WinValue = multi * BetAmount;
         totalWin += WinValue;
         if (!PerMultiplierWin.ContainsKey(multi))
@@ -178,10 +129,9 @@ public class HitAndMiss
             PerMultiplierWin[multi]++;
     }
 
-    private void SpinLooseCalculation(int g_type)
+    private void SpinLooseCalculation(int gType)
     {
         WinValue = 0;
-
         if (!PerMultiplierWin.ContainsKey(WinValue))
             PerMultiplierWin.Add(WinValue, 1);
         else
@@ -192,167 +142,19 @@ public class HitAndMiss
     {
         long sum = 0;
         int weightSum = 0;
-        for (int weightSumIdx = 0; weightSumIdx < weights.Count; weightSumIdx++)
+        for (int i = 0; i < weights.Count; i++)
         {
-            weightSum += weights[weightSumIdx];
+            weightSum += weights[i];
         }
         long r = rand.Next(0, weightSum);
         for (int i = 0; i < weights.Count; i++)
         {
             sum += weights[i];
             if (r < sum)
-                return (values != null) ? values[i] : i;
+                return values != null ? values[i] : i;
         }
-        Console.Write("All weights are 0");
+        Console.WriteLine("All weights are 0");
         return -1;
-
     }
 
-    private void readdata()
-    {
-        using (var workbook = new XLWorkbook(xlFilename))
-        {
-            //Processing data from excel sheet Three_turns
-            var worksheet = workbook.Worksheet("Weight_three");
-
-            //RangeExcel.Range range;
-            var a = worksheet.Range("W_THREE");
-            IXLRange reelRange = workbook.Range("W_THREE");
-
-
-            int Row = 0;
-            foreach (var row in a.RowsUsed())
-            {
-
-
-                foreach (var cell in row.CellsUsed())
-                {
-                    W_three.Add((int)(cell.Value));
-                }
-                Row++;
-            }
-
-            var worksheet1 = workbook.Worksheet("Weight_five");
-
-            //RangeExcel.Range range;
-            var b = worksheet1.Range("W_FIVE");
-            //IXLRange reelRange = workbook.Range("W_THREE");
-
-
-            Row = 0;
-            foreach (var row in b.RowsUsed())
-            {
-                foreach (var cell in row.CellsUsed())
-                {
-                    W_five.Add((int)(cell.Value));
-                }
-                Row++;
-            }
-            var worksheet2 = workbook.Worksheet("Weight_three");
-
-            //RangeExcel.Range range;
-            var c = worksheet2.Range("THREE_WCOMBO");
-            //IXLRange reelRange = workbook.Range("W_THREE");
-
-
-            Row = 0;
-            foreach (var row in c.RowsUsed())
-            {
-                three_combo.Add(new List<int>());
-                foreach (var cell in row.CellsUsed())
-                {
-                    three_combo[Row].Add((int)(cell.Value));
-                }
-                Row++;
-            }
-
-            var worksheet5 = workbook.Worksheet("Three_Turns");
-
-            //RangeExcel.Range range;
-            var f = worksheet5.Range("FIVE_WCOMBO");
-            //IXLRange reelRange = workbook.Range("W_THREE");
-
-
-            Row = 0;
-            foreach (var row in f.RowsUsed())
-            {
-                five_combo.Add(new List<int>());
-                foreach (var cell in row.CellsUsed())
-                {
-                    five_combo[Row].Add((int)(cell.Value));
-                }
-                Row++;
-            }
-
-
-            var worksheet3 = workbook.Worksheet("Five_Turns");
-
-            //RangeExcel.Range range;
-            var d = worksheet3.Range("FIVE_WINLOSS");
-            //IXLRange reelRange = workbook.Range("W_THREE");
-
-
-            Row = 0;
-            foreach (var row in d.RowsUsed())
-            {
-                foreach (var cell in row.CellsUsed())
-                {
-                    W_fiveWL.Add((double)(cell.Value));
-                }
-                Row++;
-            }
-
-            var worksheet4 = workbook.Worksheet("Three_Turns");
-
-            //RangeExcel.Range range;
-            var e = worksheet4.Range("THREE_WINLOSS");
-            //IXLRange reelRange = workbook.Range("W_THREE");
-
-
-            Row = 0;
-            foreach (var row in e.RowsUsed())
-            {
-                foreach (var cell in row.CellsUsed())
-                {
-                    W_threeWL.Add((double)(cell.Value));
-                }
-                Row++;
-            }
-
-            var worksheet6 = workbook.Worksheet("Three_Turns");
-
-            //RangeExcel.Range range;
-            var g = worksheet6.Range("Multiplier_three");
-            //IXLRange reelRange = workbook.Range("W_THREE");
-
-
-            Row = 0;
-            foreach (var row in g.RowsUsed())
-            {
-                foreach (var cell in row.CellsUsed())
-                {
-                    M_three.Add((int)(cell.Value));
-                }
-                Row++;
-            }
-
-            var worksheet7 = workbook.Worksheet("Five_Turns");
-
-            //RangeExcel.Range range;
-            var h = worksheet7.Range("Multiplier_five");
-            //IXLRange reelRange = workbook.Range("W_THREE");
-
-
-            Row = 0;
-            foreach (var row in h.RowsUsed())
-            {
-                foreach (var cell in row.CellsUsed())
-                {
-                    M_five.Add((int)(cell.Value));
-                }
-                Row++;
-            }
-
-        }
-    }
 }
